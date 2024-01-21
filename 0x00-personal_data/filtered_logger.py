131b8f68c538795c1f059aee69793403821149bf
jason <nftsingularity8@gmail.com>
2024-01-21 17:10:34 +0000

    Log Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,17 +9,22 @@ import os
 
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
+    'replace': lambda x: r'\g<field>={}'.format(x)
 }
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str,
-) -> str:
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str,
+    ) -> str:
     """obfuscate log messages"""
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, separator), replace(redaction), message)
+    extract, replace = (patterns["extract"],
+                        patterns["replace"])
+    return re.sub(extract(fields, separator),
+                  replace(redaction), message)
 
 
 def get_logger() -> logging.Logger:
