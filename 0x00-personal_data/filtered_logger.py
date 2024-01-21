317e235a589e77f4fb473a0fb3bee1c6e67b822c
jason <nftsingularity8@gmail.com>
2024-01-21 16:17:51 +0000

    personal Data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,12 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """Filter and obfuscate specified fields in a log message."""
     for field in fields:
-        message = re.sub(f'{field}=[^ {separator}]*', f'{field}={redaction}', message)
+        message = re.sub(f'{field}=[^ {separator}]*',
+                         f'{field}={redaction}', message)
     return message
