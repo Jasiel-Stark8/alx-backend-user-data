5dcad29531892060ff59bfbfc701a07444770cc8
jason <nftsingularity8@gmail.com>
2024-01-21 17:50:25 +0000

    Password Validation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-"""Password Encryption Module"""
+"""Password Encryption and Validation Module"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """Password hashing function"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """Password Validation"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
