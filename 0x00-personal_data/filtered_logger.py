6525be1e5df00620d3c9bbcaf59ac9c646660308
jason <nftsingularity8@gmail.com>
2024-01-21 16:16:30 +0000

    personal Data

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Filter log module"""
+import logging
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Filter and obfuscate specified fields in a log message."""
+    for field in fields:
+        message = re.sub(f'{field}=[^ {separator}]*', f'{field}={redaction}', message)
+    return message
--- 0x00-personal_data/log.py
+++ 0x00-personal_data/log.py
@@ -1,10 +1,15 @@
 """Logging Module"""
 import logging
+import datetime
 
-def logging_finction() -> None:
+def logging_function() -> None:
     """Logging Function"""
-    logging.basicConfig(level=logging.WARNING)
-
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%Y-%m-%d %H-%M-%S',
+        filename='logs.log'
+    )
     logging.debug("This is a debug message")
     logging.info("This is an info message")
     logging.warning("This is a warning message")
@@ -13,4 +18,4 @@ def logging_finction() -> None:
 
 
 if __name__ == '__main__':
-    main()
+    logging_function()
