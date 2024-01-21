92b0ca49ce1b1f07e61f233c941ed63bac0612ee
jason <nftsingularity8@gmail.com>
2024-01-21 17:38:16 +0000

    Password Encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,4 +4,5 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Password hashing function"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'),
+                         bcrypt.gensalt())
